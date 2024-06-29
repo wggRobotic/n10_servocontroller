12399ccfc25e444e5c5fded0591e245e21f17797
SeaCannon1122 <coroc@buzzmail.eu>
2024-06-29 16:03:57 +0200

    redid control

--- servocontroller/servocontroller.py
+++ servocontroller/servocontroller.py
@@ -5,6 +5,8 @@ import math
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
 
+DUTY_MID = 5200
+DUTY_RANGE = 3300
 class ServoController(Node):
 
     def __init__(self):
@@ -12,9 +14,6 @@ class ServoController(Node):
         super().__init__('n10_servo_controller')
 
         self.kit = ServoKit(channels=16)
-        for i in range(9):
-            self.kit.servo[i].set_pulse_width_range(550, 2500)
-
         self.wheel_servo_channels = [0, 1, 2, 3, 4, 5]
         self.arm_servo_channels = [6, 7, 8]
 
@@ -39,16 +38,16 @@ class ServoController(Node):
         if len(msg.data) == 6:
             for i, angle in enumerate(msg.data):
                 if 0 <= i < len(self.wheel_servo_channels):
-                    self.move_to_angle(self.wheel_servo_channels[i], 180*angle/math.pi+90)
+                    self.move_to_angle(self, self.wheel_servo_channels[i], angle)
 
     def arm_control_callback(self, msg):
         if len(msg.data) == 3:
             for i, angle in enumerate(msg.data):
                 if 0 <= i < len(self.arm_servo_channels):
-                    self.move_to_angle(self.arm_servo_channels[i], 180*angle/math.pi+90)
+                    self.move_to_angle(self, self.arm_servo_channels[i], angle)
 
     def move_to_angle(channel, angle):
-        self.kit.servo[channel].angle = angle
+        self.kit.servo[channel]._pwm_out.duty_cycle = int(DUTY_MID + DUTY_RANGE * angle * 2 / math.pi)
 
 
 def main(args=None):
